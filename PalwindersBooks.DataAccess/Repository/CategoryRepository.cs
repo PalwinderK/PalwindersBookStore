2443e3583da6cc90f2b3dc96e539810bc418f691
PalwinderK <w0771480@myscc.ca>
2021-11-09 17:34:53 -0500

    Made some more modifications

---
+++ PalwindersBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using PalwindersBooks.DataAccess.Repository.IRepository;
+using PalwindersBooks.Models;
+using PalwindersBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace PalwindersBooks.DataAccess.Repository
+{
+    public class  CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
