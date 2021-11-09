4953c76d27b134985ef59eb6e3d050eff156f57b
PalwinderK <w0771480@myscc.ca>
2021-11-09 02:57:09 -0500

    Removed using statements

--- PalwindersBookStore/Startup.cs
+++ PalwindersBookStore/Startup.cs
@@ -1,4 +1,4 @@
-using PalwindersBookStore.Data;
+//using PalwindersBookStore.Data;
 using PalwindersBookStore.DataAccess.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
