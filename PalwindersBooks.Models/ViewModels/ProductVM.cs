1b43b2d676cf8388ba0454a9a0da24dd5e69db81
PalwinderK <w0771480@myscc.ca>
2021-11-22 05:20:01 -0500

    Modified ProductVM and Upsert.cshtml

--- PalwindersBooks.Models/ViewModels/ProductVM.cs
+++ PalwindersBooks.Models/ViewModels/ProductVM.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿//using PalwindersBookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 //using System.Linq;
