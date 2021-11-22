f662c8afabc8347d0ea69d3423656d85df4b5f5f
PalwinderK <w0771480@myscc.ca>
2021-11-22 02:20:28 -0500

    Added migrations

--- PalwindersBooks.DataAccess/Migrations/20211122072000_addCoverTypeToDb.Designer.cs
+++ PalwindersBooks.DataAccess/Migrations/20211122072000_addCoverTypeToDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PalwindersBookStore.DataAccess.Data;
 
 namespace PalwindersBooks.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211122072000_addCoverTypeToDb")]
+    partial class addCoverTypeToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
